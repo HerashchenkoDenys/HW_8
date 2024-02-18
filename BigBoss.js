09cb62c831ea59bd313a0419d9e07dbc0a654ae4
HerashchenkoDenys <157643762+HerashchenkoDenys@users.noreply.github.com>
2024-02-19 00:08:38 +0200

    Update BigBoss.js

--- BigBoss.js
+++ BigBoss.js
@@ -1,1 +1,61 @@
+const company = {
+    name: 'Велика Компанія',
+    type: 'Головна компанія',
+    platform: 'Платформа для продажу квитків',
+    sellsSolution: 'Рішення для продажу квитків',
+    clients: [
+        {
+            name: 'Клієнт 1',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+            partners: [
+                {
+                    name: 'Клієнт 1.1',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                },
+                {
+                    name: 'Клієнт 1.2',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                    partners: [
+                        {
+                            name: 'Клієнт 1.2.3',
+                            type: 'subSubCompany',
+                            uses: 'Рішення для продажу квитків',
+                            sells: 'Рішення для продажу квитків',
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Клієнт 2',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків'
+        }
+    ]
+};
 
+function findValueByKey(obj, companyName) {
+    if (obj.name === companyName) {
+        return obj;
+    } else if (obj.clients) {
+        let result = null;
+        obj.clients.forEach(client => {
+            if (!result) {
+                result = findValueByKey(client, companyName);
+            }
+        });
+        return result;
+    }
+    return null;
+}
+
+// Приклад використання функції для пошуку інформації про підкомпанію
+const subCompanyInfo = findValueByKey(company, 'Клієнт 1.2');
+console.log(subCompanyInfo);
